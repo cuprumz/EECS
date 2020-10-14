6644aa381caac8d01225fbd18f14d1eae8ce463b
cuprumz@Reiner <cuprumzhen@gmail.com>
2020-10-14 17:08:06 +0800

    feat concurrency

---
+++ concurrency/README.md
@@ -0,0 +1,9 @@
+### Concurrency并发
+
+- mutual exclusion
+- [spinlocks](https://en.wikipedia.org/wiki/Spinlock)
+- deadlocks
+- resource starvation
+- [compare-and-set](https://en.wikipedia.org/wiki/Compare-and-swap) (CAS)
+- [Busy waiting](https://en.wikipedia.org/wiki/Busy_waiting), busy-looping or spinning
+- [condition variables](https://en.wikipedia.org/wiki/Monitor_(synchronization)#Condition_variables)
---
+++ src/busy_waiting_ex1.c
@@ -0,0 +1,48 @@
+#include <pthread.h>
+#include <stdatomic.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+atomic_int i = 0;
+
+static void *f1(void *p) {
+    int local_i;
+
+    while((local_i = atomic_load(&i)) == 0) {
+        /* do nothing - just keeping checking over and over */
+    }
+
+    printf("i's value has changed to %d.\n", local_i);
+    return NULL;
+}
+
+static void *f2(void *p) {
+    int local_i = 99;
+    sleep(10); /* sleep for 10 seconds */
+    atomic_store(&i, local_i);
+    printf("t2 has changed the value of i to %d.\n", local_i);
+    return NULL;
+}
+
+int main() {
+    int rc;
+    pthread_t t1, t2;
+
+    rc = pthread_create(&t1, NULL, f1, NULL);
+    if (rc != 0) {
+        fprintf(stderr, "pthread f1 failed\n");
+	return EXIT_FAILURE;
+    }
+
+    rc = pthread_create(&t2, NULL, f2, NULL);
+    if (rc != 0) {
+        fprintf(stderr, "pthread f2 failed\n");
+	return EXIT_FAILURE;
+    }
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+    puts("All thread finished.");
+    return 0;
+}
